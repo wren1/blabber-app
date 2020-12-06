abb156089615cef4b6d67910a872672875053b2a
Wren McPherson <wren.mcpherson@yahoo.com>
2020-12-05 19:09:03 -0500

    working on seeders

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.group_routes import group_routes
+from .api.invite_routes import invite_routes
+from .api.post_routes import post_routes
 
 from .seeds import seed_commands
 
@@ -31,6 +34,9 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(group_routes, url_prefix='/api/groups')
+app.register_blueprint(post_routes, url_prefix='/api/posts')
+app.register_blueprint(invite_routes, url_prefix='/api/invites')
 db.init_app(app)
 Migrate(app, db)
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash
 from app.models import db, User
 
+
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
@@ -9,12 +10,31 @@ def seed_users():
 
     db.session.add(demo)
 
+    user_1 = User(username='Demo', email='demo@aa.io',
+                  password='password', name='demo user', description='demo user', 
+                  icon_url='https://cdn2.iconfinder.com/data/icons/animals-nature-2/50/1F433-spouting-whale-512.png')
+    user_2 = User(username='seeduser1', email='email1@aa.io',
+                  password='password', name='sara', description='hi im a seed user', 
+                  icon_url='https://cdn2.iconfinder.com/data/icons/animals-nature-2/50/1F433-spouting-whale-512.png')
+    user_3 = User(username='seeduser2', email='email2@aa.io',
+                  password='password', name='sally', description='my name is sally and i like cats', 
+                  icon_url='https://cdn2.iconfinder.com/data/icons/animals-nature-2/50/1F433-spouting-whale-512.png')
+    user_4 = User(username='seeduser3', email='email3@aa.io',
+                  password='password', name='sue', description='im not an actual person', 
+                  icon_url='https://cdn2.iconfinder.com/data/icons/animals-nature-2/50/1F433-spouting-whale-512.png')
+    user_1 = User(username='seeduser4', email='email4@aa.io',
+                  password='password', name='sammy', description='what up dogaroo', 
+                  icon_url='https://cdn2.iconfinder.com/data/icons/animals-nature-2/50/1F433-spouting-whale-512.png')
+    
+
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
+
+
 def undo_users():
     db.session.execute('TRUNCATE users;')
     db.session.commit()
