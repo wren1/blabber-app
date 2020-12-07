620dd5083975aacc9cb419739685420608836b67
Wren McPherson <wren.mcpherson@yahoo.com>
2020-12-07 12:38:17 -0500

    fixed user routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, session, request
 from flask_login import login_required, current_user
-from app.models import User, Post, Group, Invite, Comment
+from app.models import User, Post, Group, Invite, Comment, Friend
 
 user_routes = Blueprint('users', __name__)
 
@@ -25,36 +25,42 @@ def user(id):
 # @login_required
 def user_posts(id):
     user = User.query.get(id)
-    user_ids = [friend.id for friend in user.friends]
+    friends = Friend.query.filter((Friend.user_one_id == id) | (Friend.user_two_id == id))
+    user_ids = [friend.id for friend in friends]
     user_ids.append(id)
+    print(user_ids)
     group_ids = [group.id for group in user.groups]
     posts = Post.query.filter((Post.user_id.in_(user_ids)) | (Post.group_id.in_(group_ids))).order_by(Post.created_on).all()
-    return posts.to_dict()
+    return {"posts": [post.to_dict() for post in posts]}
 
 
-# get all users that are the current user's friends
+# get all users that are the user's friends
 @user_routes.route('/<int:id>/friends', strict_slashes=False)
 # @login_required
 def user_friends(id):
     # user = User.query.get(id)
     friends = Friend.query.filter((Friend.user_one_id == id) | (Friend.user_two_id == id)).all()
-    return friends.to_dict()
+    return {"friends": [friend.to_dict() for friend in friends]}
 
 
-# get all groups current user is in
+# get all groups user is in
 @user_routes.route('/<int:id>/groups', strict_slashes=False)
 # @login_required
 def user_groups(id):
     user = User.query.get(id)
-    groups = Group.query.filter(Group.id.in_(user.groups)).all()
-    return group.to_dict()
+    return {"groups": [group.to_dict() for group in user.groups]}
 
 
-# get all invites to and from current user
+# get all invites to and from user
 @user_routes.route('/<int:id>/invites', strict_slashes=False)
 # @login_required
 def user_invites(id):
-    received_invites = Invite.query.filter(Invite.invitee_id == id).all()
-    sent_invites = Invite.query.filter(Invite.inviter_id == id).all()
+    user = User.query.get(id)
+    received = user.received_invites
+    sent = user.sent_invites
+    return {"received_invites": [invite.to_dict() for invite in received],
+            "sent_invites": [invite.to_dict() for invite in sent]}
+    # received_invites = Invite.query.filter(Invite.invitee_id == id).all()
+    # sent_invites = Invite.query.filter(Invite.inviter_id == id).all()
     # send info about invites too!!
-    return {"received_invites": received_invites.to_dict(), "sent_invites": sent_invites.to_dict()}
+    # return {"received_invites": received_invites.to_dict(), "sent_invites": sent_invites.to_dict()}
