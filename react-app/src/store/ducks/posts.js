e04fac7cbea8fb6a1925655430aec10bc19b25ac
Wren McPherson <wren.mcpherson@yahoo.com>
2020-12-07 14:16:53 -0500

    working on store

--- react-app/src/store/ducks/posts.js
+++ react-app/src/store/ducks/posts.js
@@ -7,3 +7,80 @@
 // delete post
 // like post
 // unlike post
+
+
+export const GET_POSTS = 'blabber/posts/get';
+export const GET_USER_POSTS = 'blabber/posts/get/user';
+export const NEW_POST = 'blabber/posts/new';
+export const NEW_GROUP_POST = 'blabber/posts/new/group';
+export const EDIT_POST = 'blabber/posts/edit';
+export const DELETE_POST = 'blabber/posts/delete';
+export const LIKE_POST = 'blabber/posts/like';
+export const UNLIKE_POST = 'blabber/posts/unlike';
+
+
+export const getPosts = (posts) => ({ type: GET_POSTS, posts })
+export const getUserPosts = (posts) => ({ type: GET_USER_POSTS, posts })
+export const newPost = (post) => ({ type: NEW_POST, post })
+export const newGroupPost = (post) => ({ type: NEW_GROUP_POST, post })
+export const editPost = (post) => ({ type: EDIT_POST, post })
+export const deletePost = (postId) => ({ type: DELETE_POST, postId })
+export const likePost = (postId) => ({ type: LIKE_POST, postId })
+export const unlikePost = (postId) => ({ type: UNLIKE_POST, postId })
+
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+export const loadPosts = () => async (dispatch) => {
+
+}
+
+
+export default function posts(state = {}, action) {
+    switch (action.type) {
+        case GET_POSTS:
+            return {};
+        case GET_USER_POSTS:
+            return {};
+        case NEW_POST:
+            return {};
+        case NEW_GROUP_POST:
+            return {};
+        case EDIT_POST:
+            return {};
+        case DELETE_POST:
+            return {};
+        case LIKE_POST:
+            return {};
+        case UNLIKE_POST:
+            return {};
+        default:
+            return state;
+    }
+}
--- react-app/src/store/ducks/users.js
+++ react-app/src/store/ducks/users.js
@@ -2,3 +2,39 @@
 // get all user's friends
 // get user
 // get all users in group
+
+export const GET_FRIENDS = 'blabber/users/friends';
+export const GET_USER = 'blabber/users/user';
+export const GET_GROUP_MEMBERS = 'blabber/users/groupMembers';
+
+
+export const getFriends = (friends) => ({ type: GET_FRIENDS, friends })
+export const getUser = (user) => ({ type: GET_USER, user })
+export const getGroupMembers = (members) => ({ type: GET_GROUP_MEMBERS, members })
+
+
+export const loadFriends = () => async (dispatch) => {
+
+}
+
+export const loadUser = () => async (dispatch) => {
+
+}
+
+export const loadGroupMembers = () => async (dispatch) => {
+    
+}
+
+
+export default function users(state = {}, action) {
+    switch (action.type) {
+        case GET_FRIENDS:
+            return {};
+        case GET_USER:
+            return {};
+        case GET_GROUP_MEMBERS:
+            return {};
+        default:
+            return state;
+    }
+}
