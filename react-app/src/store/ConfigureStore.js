b82ae4d84847c63971599a1fb9ad9ad05fc10d35
Wren McPherson <wren.mcpherson@yahoo.com>
2020-12-07 13:34:32 -0500

    working on store

---
+++ react-app/src/store/ConfigureStore.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
+import currentUser from "./ducks/currentUser";
+import users from "./ducks/users";
+import groups from "./ducks/groups";
+import invites from "./ducks/invites";
+import posts from "./ducks/posts";
+import comments from "./ducks/comments";
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const reducer = combineReducers({
+  currentUser,
+  users,
+  groups,
+  invites,
+  posts,
+  comments
+});
+
+const configureStore = (initialState) => {
+  return createStore(
+    reducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk))
+  );
+};
+
+export default configureStore;
