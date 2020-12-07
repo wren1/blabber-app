5754c3f3ca1e1431f3ea2f7e77abc14a7d86a327
Wren McPherson <wren.mcpherson@yahoo.com>
2020-12-07 16:35:35 -0500

    working on store

--- react-app/src/store/ducks/groups.js
+++ react-app/src/store/ducks/groups.js
@@ -1,10 +1,19 @@
+export const GET_GROUP = 'blabber/groups/get';
+export const GET_USER_GROUPS = 'blabber/groups/user/get';
+export const NEW_GROUP = 'blabber/groups/new';
+export const DELETE_GROUP = 'blabber/groups/delete';
+export const EDIT_GROUP = 'blabber/groups/edit';
+export const JOIN_GROUP = 'blabber/groups/join';
+export const LEAVE_GROUP = 'blabber/groups/leave';
+export const DELETE_MOD = 'blabber/group/mods/delete';
+export const NEW_MOD = 'blabber/groups/mods/neGET_GROUP w'
 
-// get group
-// get all user's groups
-// create group
-// delete group
-// edit group
-// join group
-// remove member from group / leave group
-// remove mod
-// add new mod
+export const getGroup = () => ({type: GET_GROUP });
+export const getUserGroups = () => ({type: GET_USER_GROUPS });
+export const getGroup = () => ({type: GET_GROUP });
+export const getGroup = () => ({type: GET_GROUP });
+export const getGroup = () => ({type: GET_GROUP });
+export const getGroup = () => ({type: GET_GROUP });
+export const getGroup = () => ({type: GET_GROUP });
+export const getGroup = () => ({type: GET_GROUP });
+export const getGroup = () => ({type:  NEW_MOD });
