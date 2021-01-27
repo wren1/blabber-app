e3756f7985b44c606122a6428f759a6449efdf88
Wren McPherson <wren.mcpherson@yahoo.com>
2021-01-26 23:58:40 -0500

    clean up code

--- react-app/src/components/ProfilePage/ProfileHeader.js
+++ react-app/src/components/ProfilePage/ProfileHeader.js
@@ -3,9 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import HomeButton from '../Sidebar/HomeButton';
-// import PersonAddIcon from '@material-ui/icons/PersonAdd';
-// import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-// import PersonIcon from '@material-ui/icons/Person';
 import UserInfo from './UserInfo';
 import UserIcon from '../UserIcon';
 import ProfileIcon from '../ProfileIcon'
@@ -19,28 +16,11 @@ const ProfileHeader = ({ user }) => {
     const currentUser = useSelector(state => state.currentUser);
     const invites = useSelector(state => state.invites.sent)
     const [requested, setRequested] = useState(false)
-    // let isFriend = currentUser.friends.includes(user.id)
-    // const [isRequested, setIsRequested] = useState(friend);
     console.log('P H: ', user)
     if (!user) return null;
-
-    // const handleRequestFriend = () => {
-    //     dispatch(sendFriendRequest(user.username))
-    // }
-
-    // let isRequested = false;
-    // if (invites) {
-    //     for (let i in invites) {
-    //         if (invites[i].type === 'friend' && invites[i].invitee_id === user.id) {
-    //             isRequested = true;
-    //         }
-    //     }
-    // }
-    // const [requested, setRequested] = useState(false)
     if (invites) {
         for (let i in invites) {
             if (invites[i].type === 'friend' && invites[i].invitee_id === user.id && !requested) {
-                // isRequested = true;
                 setRequested(true)
             }
         }
