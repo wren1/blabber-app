63b2b578d59e6623fc7e29b289f4c5c7f19a2d90
Wren McPherson <wren.mcpherson@yahoo.com>
2021-03-29 23:55:36 -0400

    clean up code

--- react-app/src/components/GroupPage/InviteUser.js
+++ react-app/src/components/GroupPage/InviteUser.js
@@ -4,8 +4,6 @@ import { useParams } from 'react-router-dom';
 
 import Username from './Username';
 
-import { sendGroupInvite } from '../../store/ducks/invites';
-
 
 const InviteUser = ({ openInvite, setOpenInvite, group, user, users }) => {
     const dispatch = useDispatch();
@@ -18,39 +16,13 @@ const InviteUser = ({ openInvite, setOpenInvite, group, user, users }) => {
         }
     }
 
-    console.log('friends: ', friends)
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(sendGroupInvite(username, group.id))
-        setOpenInvite(!openInvite)
-    }
-    const handleCancel = (e) => {
-        e.preventDefault();
-        setOpenInvite(!openInvite)
-    }
-
-    const handleBDClick = () => {
-        console.log('click on backdrop');
-        setOpenInvite(false)
-    }
-
-    const handleClick = (e) => {
-        console.log('click on invites main');
-        e.stopPropagation();
-    }
 
     return (
-        <div className='group-header__invite-backdrop' onClick={handleBDClick} >
-            <div className='group-header__invite' onClick={(e) => handleClick(e)} >
+        <div className='group-header__invite-backdrop' onClick={() => setOpenInvite(false)} >
+            <div className='group-header__invite' onClick={(e) => e.stopPropagation()} >
                 {!friends.length ? null : friends.map(friend => {
                     return <Username currentUser={user} user={friend} key={friend.id} openInvite={openInvite} setOpenInvite={setOpenInvite} group={group} />
                 })}
-                {/* <input className='invite-user-input' placeholder='username' value={username} name='username' onChange={(e) => setUsername(e.target.value)}/> */}
-                {/* <div className='buttons'> */}
-                        {/* <button onClick={handleCancel} className='button'>Cancel</button>
-                        <button type='submit' className='button'>Submit</button> */}
-                {/* </div> */}
             </div>
         </div>
     )
