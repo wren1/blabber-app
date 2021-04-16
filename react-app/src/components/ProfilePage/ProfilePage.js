79d8361a7d68c1650f40060ecbfa46cc76f308be
Wren McPherson <wren.mcpherson@yahoo.com>
2021-04-16 00:03:53 -0400

    update styling

--- react-app/src/components/ProfilePage/ProfilePage.js
+++ react-app/src/components/ProfilePage/ProfilePage.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import ProfileHeader from './ProfileHeader';
 import ProfileFeed from './ProfileFeed';
 import Sidebar from '../Sidebar/Sidebar';
+import Loading from '../Loading';
 
 import { loadUserPosts } from '../../store/ducks/posts';
 import { loadUser } from '../../store/ducks/users';
@@ -13,24 +14,27 @@ import { loadInvites } from '../../store/ducks/invites';
 
 
 const ProfilePage = () => {
+    const [loaded, setLoaded] = useState(false)
     const dispatch = useDispatch();
     let { userId } = useParams();
     
     const user = useSelector(state => state.users[`"${userId}"`]);
     const currentUser = useSelector(state => state.currentUser)
-
+    
     useEffect(() => {
         (async () => {
             await dispatch(loadUser(userId))
             await dispatch(loadUserPosts(userId))
             await dispatch(loadInvites())
+            setLoaded(true)
         })();
     }, []);
     
     console.log(userId)
-
+    
     return (
         <div className='profile'>
+            {!loaded ? <Loading /> : null}
             <Sidebar user={currentUser} />
             <ProfileHeader user={user} />
             <ProfileFeed />
