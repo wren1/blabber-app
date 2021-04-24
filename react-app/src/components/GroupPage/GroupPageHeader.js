2a727cfc2a718928ded5b5bdf99cdb1d69d8535d
Wren McPherson <wren.mcpherson@yahoo.com>
2021-04-23 22:16:16 -0400

    clean up code

--- react-app/src/components/GroupPage/GroupPageHeader.js
+++ react-app/src/components/GroupPage/GroupPageHeader.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import GroupDetails from './GroupDetails';
 import GroupHeader from './GroupHeader';
 
 
 const GroupPageHeader = ({ groupId, user }) => {
-    const dispatch = useDispatch();
     const group = useSelector(state => state.groups[`"${groupId}"`])
     const users = useSelector(state => state.users)
+
     if (!group) return null;
-    console.log('GROUP DESC: ', group)
+
     return (
         <div className='group-header'>
             <GroupHeader group={group} user={user} users={users} />
--- react-app/src/components/ProfilePage/UserInfo.js
+++ react-app/src/components/ProfilePage/UserInfo.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-import PersonIcon from '@material-ui/icons/Person';
-import PersonAddDisabledIcon from '@material-ui/icons/PersonAddDisabled';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import ProfileIcon from '../ProfileIcon';
 import FriendIcon from './FriendIcon';
 
-import { sendFriendRequest } from '../../store/ducks/invites'
-import { removeFriend } from '../../store/ducks/users';
-
 
 const UserInfo = ({ user, requested, setRequested }) => {
-    const dispatch = useDispatch();
     const currentUser = useSelector(state => state.currentUser)
 
     if (!user) return null;
