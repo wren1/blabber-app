f656562bc88155a661bdec1b646bd35946b04e4e
Wren McPherson <wren.mcpherson@yahoo.com>
2021-04-30 23:45:01 -0400

    update styling

--- react-app/src/components/Posts/CommentHeader.js
+++ react-app/src/components/Posts/CommentHeader.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import ProfileIcon from '../ProfileIcon';
 import Link from '../Link';
 
 
--- react-app/src/components/Posts/Comments.js
+++ react-app/src/components/Posts/Comments.js
@@ -1,38 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Comment from './Comment';
 import CommentForm from './CommentForm';
 
 
-const Comments = ({ post, commentIds, users }) => {
+const Comments = ({ post, commentIds }) => {
     const allComments = useSelector(state => state.comments)
-    const posts = useSelector(state => state.posts)
-    // commentIds = posts[`"${post.id}"`].comments;
+
     const comments = [];
-    // const [comments, setComments] = useState([])
-    console.log('ids: ', commentIds)
+
     if (commentIds) {
-        console.log('all: ', allComments)
         commentIds.forEach(id => comments.push(allComments[`"${id}"`]))
     }
-
-
-    console.log('COMMENTS: ', comments)
-    // if (!commentIds) {
-    //     return (
-    //         <div className='comments-container'>
-    //             <CommentForm post={post}/>
-    //         </div>
-    //     );
-    // } else {
-
     if (commentIds.length !== comments.length) return null;
 
     return (
         <div className='comments-container'>
             
-            {!commentIds ? 'hi' : comments.map(comment => 
+            {!commentIds ? null : comments.map(comment => 
                 (
                     <Comment comment={comment} key={comment.id} />
                 ))}
